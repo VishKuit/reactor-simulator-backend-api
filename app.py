2f0aec0bc7ba6167c4f6cd704290da6947205aba
JESUS D. GARCIA <jesus.garcia@nivelics.co>
2024-04-10 18:24:25 -0500

    Avances reunión 10 de abrl

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
 
+from example_blueprint import example_blueprint
+
+
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_geek():
-    return "<h2>Hello from Flask & Docker</h2>"
+# @app.route("/")
+# def hello_geek():
+#     return "<h2>Hello from Flask & Docker</h2>"
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+
+app.register_blueprint(example_blueprint)
 
 
 if __name__ == "__main__":
